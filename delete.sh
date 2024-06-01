74fc0b491a6d9c9803d3ce60e25a9b11b2708ff1
walidlabied09 <walid.labied@uir.ac.ma>
2024-06-01 12:59:14 +0100

    Delete delete.sh

--- delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-deleteTaskById() {
-    local taskId="$1"
-    local tempFile="./tempFile.txt"
-    grep -v "id= $taskId" file.txt > "$tempFile"
-    mv "$tempFile" file.txt
-    echo "Task with id $taskId has been deleted."
-}
